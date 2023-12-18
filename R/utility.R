c65ddbf9d9a8029dd49ea195d60cde14cadfe980
ChiLiubio <liuchi0426@126.com>
2023-12-18 11:21:28 +0800

    fix tidy_taxonomy func wrong match issue in pattern param

--- R/utility.R
+++ R/utility.R
@@ -150,12 +150,13 @@ trycharnum <- function(x){
 #'
 #' @param taxonomy_table a data.frame with taxonomic information.
 #' @param column default "all"; "all" or a number; 'all' represents cleaning up all the columns; a number represents cleaning up this column.
-#' @param pattern default see the function parameter; the characters (regular expression) to be cleaned up or replaced; cleaned up when parameter replacement = "", 
+#' @param pattern default c(".*unassigned.*", ".*uncultur.*", ".*unknown.*", ".*unidentif.*", ".*unclassified.*", ".*No blast hit.*", ".*Incertae.sedis.*"); 
+#'   the characters (regular expressions) to be removed or replaced; removed when parameter \code{replacement = ""}, 
 #'   replaced when parameter replacement has something; Note that the capital and small letters are not distinguished when \code{ignore.case = TRUE}.
-#' @param replacement default ""; the characters used to replace the character in pattern parameter.
+#' @param replacement default ""; the characters used to replace the character in \code{pattern} parameter.
 #' @param ignore.case default TRUE; if FALSE, the pattern matching is case sensitive and if TRUE, case is ignored during matching.
-#' @param na_fill default ""; used to replace the NA.
-#' @return taxonomic table.
+#' @param na_fill default ""; used to replace \code{NA}.
+#' @return data.frame
 #' @format \code{\link{data.frame}} object.
 #' @examples
 #' data("taxonomy_table_16S")
@@ -163,8 +164,7 @@ trycharnum <- function(x){
 #' @export
 tidy_taxonomy <- function(taxonomy_table, 
 	column = "all",
-	pattern = c(".*Unassigned.*", ".*uncultur.*", ".*unknown.*", ".*unidentif.*", ".*unclassified.*", ".*No blast hit.*", ".*sp\\.$",
-		".*metagenome.*", ".*cultivar.*", ".*archaeon$", "__synthetic.*", ".*\\sbacterium$", ".*bacterium\\s.*", ".*Incertae.sedis.*"),
+	pattern = c(".*unassigned.*", ".*uncultur.*", ".*unknown.*", ".*unidentif.*", ".*unclassified.*", ".*No blast hit.*", ".*Incertae.sedis.*"),
 	replacement = "",
 	ignore.case = TRUE,
 	na_fill = ""
